847dfe0e77ca1cc3c3c4bbd7f682a94ddb44395e
Joseph Marsden <josephmarsden@towerdevs.xyz>
2019-03-01 08:23:55 +0000

    Added packageinfo

---
+++ ValveMultitool/Models/Formats/Steam/Distribution/Packages/PackageInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ValveMultitool.Models.Formats.Steam.Distribution.Packages
+{
+    public class PackageInfo
+    {
+        public int PackageId { get; set; }
+        public int BillingType { get; set; }
+        public int LicenseType { get; set; }
+        public int Status { get; set; }
+        public Dictionary<string, string> Extended { get; set; }
+        public Dictionary<int, int> AppIds { get; set; }
+        public Dictionary<int, int> DepotIds { get; set; }
+        public Dictionary<string, string> AppItems { get; set; }
+    }
+}
